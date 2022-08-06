827d8d18ee7be1baca9f151756186e31d0daac53
Marcelo Santos <rocha.mer21@gmail.com>
2022-08-05 22:09:15 -0300

    Stage 4/7: stage4

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -10,20 +10,51 @@ fun main() {
     repeat(numberOfCards) {
 
         println("Card #$count:")
-        val card = readln()
+        val card = readln().let {
+            var temp = it
+            while (true) {
+                if (cards.containsKey(temp)) {
+                    println("The term \"$it\" already exists. Try again:")
+                    temp = readln()
+                } else {
+                    break
+                }
+            }
+            temp
+        }
+
         println("The definition for card #$count:")
-        val definition = readln()
+        val definition = readln().let {
+            var temp = it
+            while (true) {
+                if (cards.containsValue(temp)) {
+                    println("The definition \"$it\" already exists. Try again:")
+                    temp = readln()
+                } else {
+                    break
+                }
+            }
+            temp
+        }
 
         cards[card] = definition
-
         count++
     }
+
 
     for ((i, x) in cards) {
         println("Print the definition of \"$i\":")
         val definition = readln()
         if (definition == x) println("Correct!")
-        else println("Wrong. The right answer is \"$x\".")
+        else {
+            if (cards.containsValue(x)) {
+                println(
+                    "Wrong. The right answer is \"$x\", but your definition is correct for \"${
+                        cards.filterValues { definition == it }.keys
+                    }\".".replace("(\\[|\\])".toRegex(), "")
+                )
+            } else println("Wrong. The right answer is \"$x\".")
+        }
     }
 
 }
